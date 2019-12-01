75fda0771477dea760771532783be1d236821abc
Alexander Berg <alexander.berg.p92@sonymusic.com>
2019-12-01 14:49:40 +0100

    - initial commit, added thesis, i18n

--- app/src/main/java/org/semmelweis/university/anatomyapp/ui/learn/LearnFragment.java
+++ app/src/main/java/org/semmelweis/university/anatomyapp/ui/learn/LearnFragment.java
@@ -2,33 +2,25 @@ package org.semmelweis.university.anatomyapp.ui.learn;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import org.semmelweis.university.anatomyapp.R;
 
-import java.util.Objects;
-
 public class LearnFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_learn, container, false);
-        final AutoCompleteTextView autoCompleteTextView=root.findViewById(R.id.autoCompleteTextView);
-        autoCompleteTextView.setAdapter(new ArrayAdapter<>(Objects.requireNonNull(getActivity()), android.R.layout.simple_list_item_1, LearnConstants.THESISES));
-        autoCompleteTextView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                autoCompleteTextView.showDropDown();
-                return false;
-            }
-        });
+        final View root = inflater.inflate(R.layout.fragment_learn, container, false);
+        Spinner thesisSpinner=root.findViewById(R.id.thesis_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.thesis, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        thesisSpinner.setAdapter(adapter);
         return root;
     }
 }
