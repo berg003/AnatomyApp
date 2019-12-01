fb675405f4690854fa7ee3e121cc345acec5afb9
Alexander Berg <alexander.berg.p92@sonymusic.com>
2019-12-01 09:45:52 +0100

    - initial commit

--- app/src/main/java/org/semmelweis/university/anatomyapp/ui/learn/LearnFragment.java
+++ app/src/main/java/org/semmelweis/university/anatomyapp/ui/learn/LearnFragment.java
@@ -2,6 +2,7 @@ package org.semmelweis.university.anatomyapp.ui.learn;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -19,8 +20,15 @@ public class LearnFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_learn, container, false);
-        AutoCompleteTextView autoCompleteTextView=root.findViewById(R.id.autoCompleteTextView);
+        final AutoCompleteTextView autoCompleteTextView=root.findViewById(R.id.autoCompleteTextView);
         autoCompleteTextView.setAdapter(new ArrayAdapter<>(Objects.requireNonNull(getActivity()), android.R.layout.simple_list_item_1, LearnConstants.THESISES));
+        autoCompleteTextView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                autoCompleteTextView.showDropDown();
+                return false;
+            }
+        });
         return root;
     }
 }
